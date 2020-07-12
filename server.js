9e44f04e8d33563be3a38f17fa84910ca7741d72
Fernando Frazão <ffrazao@gmail.com>
2020-07-12 08:48:20 -0300

    heroku
    Preparar pra subir
    
    Signed-off-by: Fernando Frazão <ffrazao@gmail.com>

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+const nomeApp = process.env.npm_package_name;
 
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/voteaqui'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', function(req,res) {
     console.log('executando', req.path);
-    res.sendFile(path.join(__dirname+'/dist/voteaqui/index.html'));
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 // Inicia a aplicação pela porta configurada
