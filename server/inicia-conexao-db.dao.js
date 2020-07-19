b9731fed42a4caa07fd098cd30d75f40f3c211a4
Fernando Frazão <ffrazao@gmail.com>
2020-07-19 12:18:53 -0300

    Ajustes
    preparaçao para produção
    banco removido do controle de versoes
    
    Signed-off-by: Fernando Frazão <ffrazao@gmail.com>

--- server/inicia-conexao-db.dao.js
+++ server/inicia-conexao-db.dao.js
@@ -1,38 +1,78 @@
-const Promise = require('bluebird');
+const Promise = require("bluebird");
 
-const Dao = require('./dao/dao');
-const VotacaoDao = require('./dao/votacao.dao');
-const PautaDao = require('./dao/pauta.dao');
-const OpcaoDao = require('./dao/opcao.dao');
-const ParticipanteDao = require('./dao/participante.dao');
-const VotoDao = require('./dao/voto.dao');
+const Dao = require("./dao/dao");
+const VotacaoDao = require("./dao/votacao.dao");
+const PautaDao = require("./dao/pauta.dao");
+const OpcaoDao = require("./dao/opcao.dao");
+const ParticipanteDao = require("./dao/participante.dao");
+const VotoDao = require("./dao/voto.dao");
 
-const conexaoDbDao = new Dao('./server/db.sqlite3');
+const conexaoDbDao = new Dao("./server/db.sqlite3");
 
 function iniciarBancoDeDados() {
-  console.log('iniciando banco de dados');
+  console.log("iniciando banco de dados");
   const votacaoDao = new VotacaoDao(conexaoDbDao);
   const pautaDao = new PautaDao(conexaoDbDao);
   const opcaoDao = new OpcaoDao(conexaoDbDao);
   const participanteDao = new ParticipanteDao(conexaoDbDao);
   const votoDao = new VotoDao(conexaoDbDao);
 
-  votacaoDao.createTable().then(() => console.log(`Tabela Votacao verificada!`)).catch((err) => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-  });
-  pautaDao.createTable().then(() => console.log(`Tabela Pauta verificada!`)).catch((err) => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-  });
-  opcaoDao.createTable().then(() => console.log(`Tabela Opcao verificada!`)).catch((err) => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-  });
-  participanteDao.createTable().then(() => console.log(`Tabela Participante verificada!`)).catch((err) => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-  });
-  votoDao.createTable().then(() => console.log(`Tabela Voto verificada!`)).catch((err) => {
-    console.log(`Error: ${JSON.stringify(err)}`)
-  });
-
+  votacaoDao
+    .createTable()
+    .then(() => {
+      console.log(`Tabela Votacao verificada!`);
+      votacaoDao.dao
+        .run(`CREATE UNIQUE INDEX IF NOT EXISTS VotacaoUq ON Votacao(codigo)`)
+        .then(() => console.log(`Indice VotacaoUq verificado!`))
+        .catch((err) => {
+          console.log(`Error: ${JSON.stringify(err)}`);
+        });
+    })
+    .catch((err) => {
+      console.log(`Error: ${JSON.stringify(err)}`);
+    });
+  pautaDao
+    .createTable()
+    .then(() => {
+      console.log(`Tabela Pauta verificada!`);
+      pautaDao.dao
+        .run(
+          `CREATE UNIQUE INDEX IF NOT EXISTS PautaUq ON Pauta(votacaoId, codigo)`
+        )
+        .then(() => console.log(`Indice PautaUq verificado!`))
+        .catch((err) => {
+          console.log(`Error: ${JSON.stringify(err)}`);
+        });
+    })
+    .catch((err) => {
+      console.log(`Error: ${JSON.stringify(err)}`);
+    });
+  opcaoDao
+    .createTable()
+    .then(() => {
+      console.log(`Tabela Opcao verificada!`);
+      pautaDao.dao
+        .run(`CREATE UNIQUE INDEX IF NOT EXISTS OpcaoUq ON Opcao(pautaId, codigo)`)
+        .then(() => console.log(`Indice OpcaoUq verificado!`))
+        .catch((err) => {
+          console.log(`Error: ${JSON.stringify(err)}`);
+        });
+    })
+    .catch((err) => {
+      console.log(`Error: ${JSON.stringify(err)}`);
+    });
+  participanteDao
+    .createTable()
+    .then(() => console.log(`Tabela Participante verificada!`))
+    .catch((err) => {
+      console.log(`Error: ${JSON.stringify(err)}`);
+    });
+  votoDao
+    .createTable()
+    .then(() => console.log(`Tabela Voto verificada!`))
+    .catch((err) => {
+      console.log(`Error: ${JSON.stringify(err)}`);
+    });
 }
 
 iniciarBancoDeDados();
