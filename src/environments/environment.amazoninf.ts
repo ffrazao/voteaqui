1ba3665e6ac3779b684e45d935eadf2a4da82ce9
Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>
2020-08-07 11:43:39 -0300

    ajuste de ambientes
    Signed-off-by: Fernando Frazão da Silva <fernando.frazao@emater.df.gov.br>

--- src/environments/environment.amazoninf.ts
+++ src/environments/environment.amazoninf.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://45.4.186.2:8080/'
+  API_URL: 'http://45.4.186.2:8080'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // API_URL: 'https://voteaqui.herokuapp.com'
-  API_URL: 'http://45.4.186.2:8080/'
+  API_URL: 'https://voteaqui.herokuapp.com'
+  // API_URL: 'http://45.4.186.2:8080'
 };
